13daed3db901b10f359ce4f021476719a3c571bd
Tensen <shentensen@outlook.com>
2016-04-01 02:16:09 -0500

    第2章

---
+++ ch02/main.cpp
@@ -0,0 +1,100 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by ShenTensen on 15/10/17.
+//  Copyright © 2015年 ShenTensen. All rights reserved.
+//
+
+#include <iostream>
+
+int main ()
+{
+/*  //
+    int i = 0, &ri = i;
+    i = 5; ri = 10;
+    std::cout << i << " " << ri << std::endl;
+*/
+/*
+    // 练习2.21
+    int i = 0;
+    double * dp = &i;
+    int * ip = i;
+    int * p = &i;
+*/
+    // 练习2.22
+//    int j = 0;
+//    int * i ＝ &j;
+//    if (i)
+//        printf("%p", i);
+  
+    // 练习2.24
+//    int i = 42;
+//    void * p = &i;
+//    long * lp = &i;
+
+    // 练习2.26
+//    const int buf;        // 需要初始化
+//    int cnt = 0;
+//    const int sz = cnt;
+//    ++cnt;
+//    ++sz;
+
+    // 练习2.28
+//    int i, * const cp;
+//    int * p1, * const cp2;
+//    const int ic; &r = ic;
+//    const int * const p3;
+//    const int * p;
+
+    // 练习2.29
+    
+    // 练习2.30
+//    const int v2 = 0;
+//    int v1 = v2;
+//    int * p1 = &v1, &r1 = v1;
+//    const int *p2 = &v2, *const p3 = &i, &r2 = v2;
+    // 练习2.32
+//    int null = 0, *p = null;
+    // 练习2.36
+//    int a = 3, b = 4;
+//    decltype(a) c = a;
+//    decltype((b)) d = a;      // 引用
+//    ++c;
+//    ++d;
+//    std::cout << a << b << c << d << std::endl;
+
+    // 练习2.37
+//    int a = 3, b = 4;
+//    decltype(a) c = a;
+//    decltype(a = b) d = a;
+//    std::cout << a << b << c << d << std::endl;
+    // 练习 2.38
+//    int a = 3, c = 0;
+//    auto b = a;
+//    decltype(c = b) d = a;
+//    std::cout << a << b << c << d << std::endl;
+
+    // 练习 2.39
+//    struct Foo {
+//        
+//    }
+    
+    // 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
