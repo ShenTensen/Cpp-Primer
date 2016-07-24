a45ee8991b7a654fe4e40024af97c897188b0841
ShenTensen <shentensen@outlook.com>
2016-07-24 09:58:23 +0800

    增加从C++ Primer网站上下载的示例代码
    即 GCC_4_7_0 的文件夹

---
+++ ch07/7_1.cpp
@@ -0,0 +1,16 @@
+//
+//  7_1.cpp
+//  ch07
+//
+//  Created by ShenTensen on 16/7/24.
+//  Copyright © 2016年 ShenTensen. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0; 
+};
